fc0e7735d930020e7ffb7657707ca14f4f98dcb7
rainbreeze <rainbreeze2@naver.com>
2024-08-29 14:26:29 +0900

    ScrollMagic III

--- main.js
+++ main.js
@@ -96,3 +96,15 @@ gsap.to('.float_3', 2, {
     y : 60,
     rotation : 22
 });
+
+const spyEls = document.querySelectorAll('.scroll-spy');
+
+spyEls.forEach(function(spyEl){
+    new ScrollMagic
+    .Scene({
+        triggerElement : spyEl,
+        triggerHook : .8
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
+});
